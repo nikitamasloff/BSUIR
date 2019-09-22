2a6c252f9991bb7b9d9721b2ebe451ac93a30543
Nikita Maslov <developer.nikita.maslov@gmail.com>
2019-09-22 22:19:43 +0300

    Implement COM port access

---
+++ src/com.py
@@ -0,0 +1,76 @@
+from serial import Serial
+
+
+BAUD_RATES = [50, 75, 110, 134, 150, 200, 300, 600, 1200,1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
+
+
+# Reference: https://buildmedia.readthedocs.org/media/pdf/pyserial/latest/pyserial.pdf
+class Com:
+
+    # Possible values for the parameter timeout which controls the behavior of read():
+    #   • timeout = None: wait forever / until requested number of bytes are received
+    #   • timeout = 0: non-blocking mode, return immediately in any case, returning zero or more, up to
+    #       the requested number of bytes
+    #   • timeout = x: set timeout to x seconds (float allowed) returns immediately when the requested
+    #       number of bytes are available, otherwise wait until the timeout expires and return all bytes that were
+    #       received until then.
+    # write() is blocking by default, unless write_timeout is set. For possible values refer to the list for
+    #       timeout above.
+    def open(self,
+             port_name,
+             baud_rate = 9600,
+             read_timeout_sec = None,
+             write_timeout_sec = None):
+        self._port = Serial(port_name,
+                            baud_rate,
+                            timeout = read_timeout_sec,
+                            write_timeout = write_timeout_sec)
+
+    # Close port immediately.
+    def close(self):
+        del self._port
+
+    # Reads bytes from specified port.
+    # Returns bytes read.
+    def read(self, size = 1):
+        return self._port.read(size = size)
+
+    # Writes bytes to specified port.
+    # Returns number of bytes written.
+    def write(self, data):
+        return self._port.write(data)
+
+    @property
+    def port_name(self):
+        return self._port.port
+
+    @port_name.setter
+    def port_name(self, port_name):
+        self._port.port = port_name
+
+    @property
+    def read_timeout_sec(self):
+        return self._port.timeout
+
+    @read_timeout_sec.setter
+    def read_timeout_sec(self, read_timeout_sec):
+        assert(read_timeout_sec >= 0)
+        self._port.timeout = read_timeout_sec
+
+    @property
+    def write_timeout_sec(self):
+        return self._port.write_timeout
+
+    @write_timeout_sec.setter
+    def write_timeout_sec(self, write_timeout_sec):
+        assert(write_timeout_sec >= 0)
+        self._port.write_timeout = write_timeout_sec
+
+    @property
+    def baud_rate(self):
+        return self._port.baudrate
+
+    @baud_rate.setter
+    def baud_rate(self, baud_rate):
+        assert(baud_rate in BAUD_RATES)
+        self._port.baudrate = baud_rate
---
+++ src/comreader.py
@@ -0,0 +1,68 @@
+from com import Com, BAUD_RATES
+from sys import argv
+import random
+import keyboard
+
+
+# Check arguments
+
+if len(argv) != 2:
+    print("One argument required: port name")
+    exit(1)
+
+port_name = argv[1]
+
+# --------------------------------------
+
+
+
+# Init port
+
+port = Com()
+port.open(port_name)
+
+# -------------------------------------------------
+
+
+
+# Utils
+
+def print_baudrate():
+    print("current baudrate: " + repr(port.baud_rate))
+
+def random_char():
+    return random.choice(string.ascii_letters)
+
+# -------------------------------------------------
+
+
+# Set hotkeys
+
+keyboard.add_hotkey('shift+1', lambda: on_shift1())
+keyboard.add_hotkey('shift+2', lambda: on_shift2())
+keyboard.add_hotkey('shift+3', lambda: on_shift3())
+
+def on_shift1():
+    global port
+    port.baud_rate = BAUD_RATES[0]
+    print_baudrate()
+
+def on_shift2():
+    global port
+    rand_index = random.randrange(1, len(BAUD_RATES) - 1)
+    port.baud_rate = BAUD_RATES[rand_index]
+    print_baudrate()
+
+def on_shift3():
+    global port
+    port.baud_rate = BAUD_RATES[-1]
+    print_baudrate()
+
+# -------------------------------------------------
+
+
+# Main
+
+while True:
+    data = port.read(size=1).decode()
+    print("READ: " + data)
